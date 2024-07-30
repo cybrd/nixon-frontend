4461a32fa45b83cea1548f54c38c97c58f8f3e57
cybrd <brysuralta@gmail.com>
2024-07-31 03:16:31 +0800

    build on github action

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import styles from "./App.module.css";
 
 const App: Component = () => {
+  const submit = (event: Event) => {
+    console.log("Done");
+    event.preventDefault();
+    event.stopPropagation;
+  };
+
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+        <form onSubmit={submit}>
+          <div>
+            <span>Username: </span>
+            <input name="username" />
+          </div>
+          <div>
+            <span>Password: </span>
+            <input name="password" type="password" />
+          </div>
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
       </header>
     </div>
   );
