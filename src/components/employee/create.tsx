791771c27c392f7370c271cb98a99b334f3aea23
cybrd <brysuralta@gmail.com>
2024-09-11 06:38:57 +0800

    change department to select field

--- src/components/employee/create.tsx
+++ src/components/employee/create.tsx
@@ -4,6 +4,7 @@ import { useContext } from "solid-js";
 
 import { AuthContext } from "../../context/auth";
 import { Employee } from "../../models/employee";
+import { FilterOptionsContext } from "../../context/filter-options";
 import { employeeCreate } from "../../services/employee";
 
 const inputFingerPrintId = (setFields: SetStoreFunction<Partial<Employee>>) => (
@@ -21,20 +22,29 @@ const inputFingerPrintId = (setFields: SetStoreFunction<Partial<Employee>>) => (
   </div>
 );
 
-const inputDepartment = (setFields: SetStoreFunction<Partial<Employee>>) => (
-  <div class="form-group row p-1 align-items-center">
-    <label for="inputDepartment" class="col-sm-2 form-label text-end">
-      Department
-    </label>
-    <div class="col-sm-4">
-      <input
-        id="inputDepartment"
-        class="form-control"
-        onInput={(e) => setFields("department", e.target.value)}
-      />
+const selectDepartment = (setFields: SetStoreFunction<Partial<Employee>>) => {
+  const filter = useContext(FilterOptionsContext);
+  const options = filter.filterOptions().department;
+
+  return (
+    <div class="form-group row p-1">
+      <label for="selectDepartment" class="col-sm-2 form-label text-end">
+        Department
+      </label>
+      <div class="col-sm-4">
+        <select
+          id="selectDepartment"
+          onChange={(e) => setFields("department", e.target.value)}
+        >
+          <option value="">----</option>
+          {Object.entries(options).map(([k, v]) => (
+            <option value={k}>{v}</option>
+          ))}
+        </select>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 const inputPosition = (setFields: SetStoreFunction<Partial<Employee>>) => (
   <div class="form-group row p-1 align-items-center">
@@ -93,7 +103,7 @@ export const Create = () => {
     <div>
       <form id="form" onSubmit={submit}>
         {inputFingerPrintId(setFields)}
-        {inputDepartment(setFields)}
+        {selectDepartment(setFields)}
         {inputPosition(setFields)}
         {inputName(setFields)}
 
